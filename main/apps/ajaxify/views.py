e31064046e20cb21b576b3b9a87e3716192a1c4d
ehoversten <ehoversten@gmail.com>
2018-10-23 22:21:22 -0400

    display saved data

--- main/apps/ajaxify/views.py
+++ main/apps/ajaxify/views.py
@@ -5,9 +5,12 @@ from .forms import PostForm
 # Create your views here.
 def index(request):
     # create an instance of the form
+    posts = Post.objects.all()
+
     form = PostForm()
     context = {
-        "form": form,
+        "form"  : form,
+        "posts" : posts,
     }
     return render(request, "ajaxify/index.html", context)
 
@@ -15,9 +18,15 @@ def index(request):
 def insert(request):
     # print("REQUEST: " + request.POST)
     if request.method == "POST":
-        print(request.POST)
-        print("*"*50)
+        # print(request.POST)
+        # print("*"*50)
+        # print("TITLE: " + request.POST['title'])
+        # print("MESSAGE: " + request.POST['message'])
+        # print("*"*50)
+
         post = Post(title=request.POST['title'], message=request.POST['message'])
         post.save()
+
+        # print(post)
     # print("POST: " + post)
     return redirect('/')
