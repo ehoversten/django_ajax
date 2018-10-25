650f34cf5071e11b33172646d6ac084a2755ab0f
ehoversten <ehoversten@gmail.com>
2018-10-25 00:37:46 -0400

    added class based views

--- main/apps/ajaxify/urls.py
+++ main/apps/ajaxify/urls.py
@@ -1,9 +1,19 @@
 from django.conf.urls import url
 from . import views
+# from views import Posts
 
 app_name = 'ajaxify'
 
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^insert$', views.insert, name='insert'),
+
+    # CBV routes
+    url(r'^posts$', views.Posts.as_view(), name='posts'),
+    url(r'^template$', views.PostTemplateView.as_view(), name='template'),
+    url(r'^form$', views.PostFormView.as_view(), name='form'),
+
+    url(r'^posty$', views.PostListView.as_view(), name='posty'),
+    url(r'^new_post$', views.NewPostView.as_view(), name='new_post'),
+
 ]
--- main/apps/ajaxify/views.py
+++ main/apps/ajaxify/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import Post
 from .forms import PostForm
 
+from django.views.generic import View, ListView, CreateView, TemplateView, FormView
+
 
 import json
 import requests
@@ -34,15 +36,71 @@ def insert(request):
         # print('New post created: ', post)
         # post.save()
 
-    # """
-    #   We do this on both GET and POST requests
-    #   Fetch all posts and render only the partial posts_index.html
-    # """
+    """
+      We do this on both GET and POST requests
+      Fetch all posts and render only the partial posts_index.html
+    """
 
-    # context = {
-    #     'posts' : Post.objects.all(),
-    # }
-    #
+    context = {
+        'posts' : Post.objects.all(),
+    }
+
     # return render(request, 'ajaxify/index.html', context)
 
     return redirect('/')
+
+
+class Posts(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Hi, There!")
+
+class PostTemplateView(TemplateView):
+    template_name = 'ajaxify/index.html'
+
+    def get_context_data(self, **kwargs):
+        form = PostForm(request.POST)
+        context = super(PostTemplateView, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.all()
+        return context
+
+class PostFormView(FormView):
+    template_name = 'ajaxify/index.html'
+    form_class = PostForm
+
+    # def form_valid(self, form):
+    #     post_ = Post.objects.create()
+    #     form.save(for_post=post_)
+    #     return redirect(post_)
+
+class NewPostView(CreateView):
+
+    def get(self, request):
+        form = PostForm(request.POST)
+        context = {
+            'posts' : Post.objects.all(),
+            'form'  : form
+        }
+        return render(request, 'ajaxify/index.html', context)
+
+
+    def post(self, request):
+        # create new post and redirest to index route
+        bound_post_form = PostForm(request.POST)
+        print('POST: ', bound_post_form)
+        if bound_post_form.is_valid():
+            # create a new post and save it to the database
+            post = Post.objects.create(title=request.POST['title'], message=request.POST['message'])
+            print('New post created: ', post)
+
+
+        return redirect('posts')
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    # queryset = Post.objects.all()
+    template_name = "ajaxify/index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(PostListView, self).get_context_data(**kwargs)
+        return context
